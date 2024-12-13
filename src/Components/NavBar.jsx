5ecb301b5959c6d40674c44563df3e1de8223f8d
Subhankar Priyaranjan <subhankar.rout@smaketsolutions.com>
2024-12-13 11:19:48 +0530

    Set AboutPage as HomePage

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -60,7 +60,7 @@ function NavBar() {
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <Link
-                  to="/"
+                  to="/home"
                   href="#"
                   className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                   aria-current="page"
@@ -68,7 +68,7 @@ function NavBar() {
                   Home
                 </Link>
               </li>
-              <li>
+              {/* <li>
                 <Link
                   to="/about-us"
                   href="#"
@@ -76,7 +76,7 @@ function NavBar() {
                 >
                   About
                 </Link>
-              </li>
+              </li> */}
               <li>
                 <a
                   href="#"
