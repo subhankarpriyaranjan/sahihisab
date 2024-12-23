0bde1f979cad7b5e7827228fc5aa1b7b0cc0150c
Subhankar Priyaranjan <subhankar.rout@smaketsolutions.com>
2024-12-23 18:08:27 +0530

    Changes for not show NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import './App.css'
-import NavBar from './Components/NavBar'
-import AboutUs from './Components/Pages/AboutUs'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import ContactUs from './Components/Pages/ContactUs'
+import { useState } from 'react';
+import './App.css';
+import NavBar from './Components/NavBar';
+import AboutUs from './Components/Pages/AboutUs';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import ContactUs from './Components/Pages/ContactUs';
 import LoginPage from './Components/Pages/LoginPage';
 import SignupPage from './Components/Pages/SignupPage';
 
@@ -15,31 +15,37 @@ import GroupExpenses from './AfterLogin/pages/GroupExpenses';
 import Reports from './AfterLogin/pages/Reports';
 
 function App() {
-  
-
   return (
     <Router>
-      <NavBar />
-      {/* <LogIn /> */}
-      <Routes>
-        <Route path="/about-us" element={<AboutUs/>} /> {/* Create a route for About Us */}
-        <Route path="/home" element={<AboutUs />} /> {/* Create a route for Home */}
-        <Route path="/log-in" element={<LoginPage/>} /> {/*  Create a route for LogIn */}
-        <Route path="/sign-up" element={<SignupPage/>} /> {/* Create a route for SignUp */}
-        <Route path="/contactus" element={<ContactUs />} /> {/* Create a route for ContactUs */}
-          <Route path='/' element={<AboutUs />} />
-      </Routes>
-      <Layout>
-        <Routes>
-        <Route path="/Dashboard" element={<Dashboard />} /> 
-           <Route path="/members" element={<Members />} /> 
-      <Route path="/expenses" element={<Expenses />} />
-         <Route path="/group-expenses" element={<GroupExpenses />} />
-         <Route path="/reports" element={<Reports />} />
-        </Routes>
-      </Layout>
+      <Main />
     </Router>
-  )
+  );
 }
 
-export default App
+function Main() {
+  const location = useLocation();
+  const hideNavBarPaths = ['/Dashboard', '/members', '/expenses', '/group-expenses', '/reports'];
+
+  return (
+    <>
+      {!hideNavBarPaths.includes(location.pathname) && <NavBar />}
+      <Routes>
+        <Route path="/about-us" element={<AboutUs />} /> {/* Create a route for About Us */}
+        <Route path="/home" element={<AboutUs />} /> {/* Create a route for Home */}
+        <Route path="/log-in" element={<LoginPage />} /> {/* Create a route for LogIn */}
+        <Route path="/sign-up" element={<SignupPage />} /> {/* Create a route for SignUp */}
+        <Route path="/contactus" element={<ContactUs />} /> {/* Create a route for ContactUs */}
+        <Route path="/" element={<AboutUs />} />
+      </Routes>
+      <Routes>
+        <Route path="/Dashboard" element={<Layout><Dashboard /></Layout>} />
+        <Route path="/members" element={<Layout><Members /></Layout>} />
+        <Route path="/expenses" element={<Layout><Expenses /></Layout>} />
+        <Route path="/group-expenses" element={<Layout><GroupExpenses /></Layout>} />
+        <Route path="/reports" element={<Layout><Reports /></Layout>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
